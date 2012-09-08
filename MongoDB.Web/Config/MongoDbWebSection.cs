ca4eea395abca1d939112a69f7f35da642eff2a4
marcosb <marcos@yuniti.com>
2012-09-08 12:33:34 -0700

    Move session state configuration to its own section

---
+++ MongoDB.Web/Config/MongoDbWebSection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+using CommonCore.Configuration;
+
+namespace MongoDB.Web.Config
+{
+	class MongoDbWebSection : AutoConfigurationSection
+	{
+		[ConfigurationProperty("connectionString", DefaultValue = "mongodb://localhost")]
+		public string ConnectionString { get; set; }
+		[ConfigurationProperty("databaseName", DefaultValue = "ASPNETDB")]
+		public string DatabaseName { get; set; }
+		[ConfigurationProperty("mongoCollectionName", DefaultValue = "SessionState")]
+		public string MongoCollectionName { get; set; }
+		[ConfigurationProperty("cacheExpireSeconds", DefaultValue = 100)]
+		public int CacheExpireSeconds { get; set; }
+		[ConfigurationProperty("maxCachedSessions", DefaultValue = 10000L)]
+		public long MaxCachedSessions { get; set; }
+	}
+}
